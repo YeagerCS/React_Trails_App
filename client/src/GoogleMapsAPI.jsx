7118fa67fc064a8b233ebba1be4a4fa2b0869aa7
tk373 <108238800+tk373@users.noreply.github.com>
2023-05-12 09:26:14 +0200

    lol

---
+++ client/src/GoogleMapsAPI.jsx
@@ -0,0 +1,6 @@
+function GoogleMapsIFrame() {
+    return {
+
+        
+    };
+}
--- client/src/TrailsForm.jsx
+++ client/src/TrailsForm.jsx
@@ -43,7 +43,6 @@ export function TrailsForm(){
               return trail;
             }
         });
-
         return updatedTrails;
     }
 
