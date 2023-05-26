a869accbddbdf49a51d9dd6cd2b94d0f521c97d8
Debug <lejsmalisi0@gmail.com>
2023-05-26 11:50:10 +0200

    Fucked up

--- client/src/Map.jsx
+++ client/src/Map.jsx
@@ -61,89 +61,63 @@ export default function Map({ location, close, dragDiv }){
             }
           );
         });
-    }
-
-    function displayRouteOnMap(route) {
+      }
+      
+      function displayRouteOnMap(route) {
         const accessToken = 'pk.eyJ1IjoibWlsa3NoYWtlcjc3NSIsImEiOiJjbGk0NmJ6ZnMwY2s4M2ZwY20xOHNxenZnIn0.rCPRzvm_TtisulAPpl4_0A';
         mapboxgl.accessToken = accessToken;
       
         const map = new mapboxgl.Map({
-          container: 'mapDiv',
-          style: 'mapbox://styles/mapbox/streets-v11',
-          center: [route.geometry.coordinates[0][0], route.geometry.coordinates[0][1]],
+          container: mapContainerRef.current,
+          style: 'mapbox://styles/mapbox/streets-v12',
+          center: [8.819174837935316, 47.22653077732017],
           zoom: 10
         });
       
-        map.on('load', () => {
-          map.addSource('route', {
-            type: 'geojson',
-            data: {
-              type: 'Feature',
-              properties: {},
-              geometry: route.geometry
-            }
-          });
-      
-          map.addLayer({
-            id: 'route',
-            type: 'line',
-            source: 'route',
-            layout: {
-              'line-join': 'round',
-              'line-cap': 'round'
-            },
-            paint: {
-              'line-color': '#888',
-              'line-width': 6
-            }
-          });
-        });
+        
       }
-
+      
       function getRoute(origin, destination, accessToken) {
-        console.log(destination);
-        console.log(origin);
         const url = `https://api.mapbox.com/directions/v5/mapbox/driving/${origin};${destination}?access_token=${accessToken}`;
         return fetch(url)
-        .then(response => response.json())
-        .then(data => {
+          .then(response => response.json())
+          .then(data => {
             // Process the response data
-            console.log(data);
-            return data.routes[0]; // Return the first route
-        })
-        .catch(error => {
+            return data; // Return the routes
+          })
+          .catch(error => {
             // Handle any errors
             console.error('Error:', error);
-        });
+          });
       }
-
-      async function activateRoute(){
+      
+      async function activateRoute() {
         const accessToken = 'pk.eyJ1IjoibWlsa3NoYWtlcjc3NSIsImEiOiJjbGk0NmJ6ZnMwY2s4M2ZwY20xOHNxenZnIn0.rCPRzvm_TtisulAPpl4_0A';
         const geocodingEndpoint = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
-            location
+          location
         )}.json?access_token=${accessToken}`;
-
+      
         let [longitude, latitude] = [0, 0];
-
-        try{
-            const response = await fetch(geocodingEndpoint)
-            const data = await response.json()
-
-            if (data.features.length > 0) {
-                [longitude, latitude] = data.features[0].center;
-            }
-        } catch(error){
-            alert(error)
+      
+        try {
+          const response = await fetch(geocodingEndpoint);
+          const data = await response.json();
+      
+          if (data.features.length > 0) {
+            [longitude, latitude] = data.features[0].center;
+          }
+        } catch (error) {
+          alert(error);
         }
-
-
-        let destination = {latitude, longitude}
+      
+        const destination = `${latitude},${longitude}`;
         getCurrentLocation().then(async data => {
-            let origin = data
-            const route = await getRoute(origin, destination, "pk.eyJ1IjoibWlsa3NoYWtlcjc3NSIsImEiOiJjbGk0NmJ6ZnMwY2s4M2ZwY20xOHNxenZnIn0.rCPRzvm_TtisulAPpl4_0A")
-            displayRouteOnMap(route)
-        })
+          const origin = `${data.latitude},${data.longitude}`;
+          const route = await getRoute(origin, destination, accessToken);
+          displayRouteOnMap(route);
+        });
       }
+      
 
     return(
         <div id="mapDiv">
